63aa92e2a55357b3d6b4ee882d96d99528c1f947
Andrew Farmer <afarmer@ittc.ku.edu>
2015-09-24 01:59:37 -0700

    Try to handle no changes.

--- travis.sh
+++ travis.sh
@@ -9,23 +9,22 @@ mastertime=`( cd _site ; git log -n1 --format="%at" )`
 
 if [ "$mastertime" -le "$sourcetime" ]
 then
-    # build hakyll
+    echo "Building hakyll"
     cabal sandbox init
     cabal update
     cabal install --only-dependencies # need to figure out how to pull archive from somewhere
 
-    # build site
+    echo "Building site"
     cabal run site -- build
 
-    # push built site up to master
+    echo "Pushing site"
     cd _site
     git status
     git diff -b
     git add --all
     git config --global user.email "xichekolas@gmail.com"
     git config --global user.name "Andrew Farmer"
-    git commit -m "travis deploy $(date '+%m/%d/%y %H:%M')"
-    git push origin master
+    ( git commit -m "travis deploy `date '+%m/%d/%y %H:%M'`" ; get push origin master) || true # in case no changes
 else
     echo "Skipping rebuild because master is already newer than source."
 fi
